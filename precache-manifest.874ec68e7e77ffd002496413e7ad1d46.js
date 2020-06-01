75285d18a083e3f7d033ab74478e39db1c7b6e04
Trung Quan Lai <LAIT07@myunitec.ac.nz>
2020-06-01 17:39:52 +1200

    Updates

---
+++ precache-manifest.874ec68e7e77ffd002496413e7ad1d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48f901d2d9085e1b4a7282a5f1f25cd",
+    "url": "/GettingStartedReact/index.html"
+  },
+  {
+    "revision": "0941e8793350be594100",
+    "url": "/GettingStartedReact/static/css/main.1b4dc8b8.chunk.css"
+  },
+  {
+    "revision": "4847b6af1b188a5d05be",
+    "url": "/GettingStartedReact/static/js/2.120c3690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GettingStartedReact/static/js/2.120c3690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0941e8793350be594100",
+    "url": "/GettingStartedReact/static/js/main.0fd0eda6.chunk.js"
+  },
+  {
+    "revision": "5dd0f2b1a15952926fcb",
+    "url": "/GettingStartedReact/static/js/runtime-main.4ef1f8e9.js"
+  }
+]);
